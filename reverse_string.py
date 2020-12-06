a1b6d90dae6265d1a3fccb98e47838f096890662
dan <dan@example.com>
2020-12-06 12:42:45 +0100

    adds reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.7
+
+# reverses the given string
+
+def reverse(w):
+   reversed_word = []
+   explode_word = list(w)
+   for i in range(len(explode_word)):
+       reversed_word.append(explode_word.pop())
+   print('Your string' + w + ' has been reversed to ', end=' ')
+   print(*reversed_word)
+
+word = 'alice'
+reverse(word)
